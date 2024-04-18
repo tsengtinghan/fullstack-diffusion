106d5ea6cc28c82b07cd81874c5cedb5621151f9
Michael Tseng <michael08120@gmail.com>
2024-04-18 01:48:04 -0700

    reformat

--- app/api/test/[slug]/route.ts
+++ app/api/test/[slug]/route.ts
@@ -1,6 +1,8 @@
-export async function POST(req:Request, { params }: { params: { slug: string }}) {
-    console.log(params.slug)
-    let res = Response.json({ body: "hi" }, { status: 201 })
-    return res
-  }
-  
+export async function POST(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  console.log(params.slug);
+  let res = Response.json({ body: "hi" }, { status: 201 });
+  return res;
+}
