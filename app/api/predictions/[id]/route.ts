39162b9fb599a410bf07b8d2f2e2dfadd704bdd2
Michael Tseng <michael08120@gmail.com>
2024-04-25 17:16:10 -0700

    fix nextjs cache

--- app/api/predictions/[id]/route.ts
+++ app/api/predictions/[id]/route.ts
@@ -1,25 +1,6 @@
-// export async function GET(req:Request, { params }: { params: { id: string }}) {
-//   const response = await fetch(
-//     "https://api.replicate.com/v1/predictions/" + params.id,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${process.env.REPLICATE_API_TOKEN}`,
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-//   let res;
-//   if (response.status !== 200) {
-//     let error = await response.json();
-//     res = Response.json({ error: error.detail }, { status: 500 })
-//     return res;
-//   }
-
-//   const prediction = await response.json();
-//   res = Response.json(prediction, { status: 200 })
-//   return res
-// }
 import Replicate from "replicate";
+
+export const revalidate = 0
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN
