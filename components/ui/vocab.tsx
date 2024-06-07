d5e47a28f861d24144383881218e047b1949e882
Michael Tseng <michael08120@gmail.com>
2024-06-07 00:01:48 -0700

    flip over

--- components/ui/vocab.tsx
+++ components/ui/vocab.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@radix-ui/react-label";
@@ -9,35 +10,43 @@ interface WordState {
   url?: string;
 }
 
-export default function Vocab({wordState} : {wordState: WordState}) {
-  return (
-    <Card className="">
-      <CardHeader>
-        <CardTitle>{wordState?.word}</CardTitle>
-      </CardHeader>
-      <CardContent className="grid w-full items-center gap-2">
-        <Label htmlFor="Definition" className="font-bold">
-          Definition
-        </Label>
-        <p className="text-sm">{wordState?.definition}</p>
-        <Label htmlFor="Example Sentence" className="font-bold">
-          Example Sentence
-        </Label>
-        <p className="text-sm">{wordState?.example}</p>
+export default function Vocab({ wordState }: { wordState: WordState }) {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-        <div className="overflow-hidden rounded-md">
-          <Image
-            src={
-              wordState.url
-                ? wordState.url
-                : "/auspicious.png"
-            }
-            alt="word"
-            width={300}
-            height={300}
-          />
+  return (
+    <div
+      className={`card ${isFlipped ? "flipped" : ""}`}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      <div className="cardContent">
+        <div className="front">
+          <div className="overflow-hidden rounded-md">
+            <Image
+              src={wordState.url ? wordState.url : "/auspicious.png"}
+              alt="word"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
-      </CardContent>
-    </Card>
+        <div className="back">
+          <Card>
+            <CardHeader>
+              <CardTitle>{wordState.word}</CardTitle>
+            </CardHeader>
+            <CardContent className="grid w-full items-center gap-2">
+              <Label htmlFor="Definition" className="font-bold">
+                Definition
+              </Label>
+              <p className="text-sm">{wordState?.definition}</p>
+              <Label htmlFor="Example Sentence" className="font-bold">
+                Example Sentence
+              </Label>
+              <p className="text-sm">{wordState?.example}</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 }
